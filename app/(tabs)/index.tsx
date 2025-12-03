5c0346daed12dac5b6b70bfb650937c1d2345dc9
Akshat Kumar <akshatkumar2022@vitbhopal.ac.in>
2025-12-03 21:59:20 +0530

    network-error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,39 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
-import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
-  const router = useRouter(); 
+  const router = useRouter();
+  
+  const { data: movies,
+     loading: moviesLoading, 
+     error: moviesError } = useFetch(() => fetchMovies({
+    query: ''
+  }))
   return (
-    <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0"/>
+    <View className="flex-1 bg-white">
+      {/* <Image source={images.bg} className="absolute w-full z-0"/> */}
       <ScrollView 
       className="flex-1 px-5" 
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{
         minHeight: "100%", 
-        paddingBottom: 10,
+        paddingBottom: 10, 
       }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        {moviesLoading ? (
+          <ActivityIndicator 
+            size="large"
+            color="0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ): (
         <View className="flex-1 mt-5">
           <SearchBar 
             onPress={() => {
@@ -25,7 +41,11 @@ export default function Index() {
             }}
             placeholder="Search for a movie"
           />
+          <>
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies: </Text>
+          </>
         </View>
+        )}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -1,41 +1,22 @@
 export const TMDB_CONFIG = {
-  BASE_URL: 'https://api.themoviedb.org/3', 
-  API_KEY: process.env.EXPO_PUBLIC_API_KEY,
+  BASE_URL: "https://api.themoviedb.org/3",
+  BEARER_TOKEN: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
-    accept: 'application/json', 
-    Authorization: 'Bearer ${process.env.EXPO_PUBLIC_API_KEY}'
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query ? `/search/movie?query=${query}` : `/discover/movie?sort_by=popularity.desc`;
+  const response = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+  if(!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies",response.statusText);
   }
-}
-
-export const fetchMovies = async ({ query } : {query: string}) => {
-  const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
-
-  const response = await fetch(endpoint, {
-    method: 'GET', 
-    headers: TMDB_CONFIG.headers, 
-  })
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch movies', response.statusText); 
-  }
-
-  const data = await response.json(); 
-
-  return data.results; 
-}
-
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTAzYTI0ZWE0MzM2YTAxNjUzMjRlMDc2ZGQzNjU1MSIsIm5iZiI6MTc1Mzc4OTQ0NC44MDgsInN1YiI6IjY4ODhiNDA0YTU3MTA4NmE0MDU1Zjk1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T2AvbqLdQtZeW99wFNAal9oN9sbAcia0zZFWD9HRJa8'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+  const data = await response.json();
+  return data.results;
+};
