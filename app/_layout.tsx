bd4821d3b5a33c6d0ea6a33dc422119d7ee2b41e
Akshat Kumar <akshatkumar2022@vitbhopal.ac.in>
2025-12-11 01:13:20 +0530

    hide-status-bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,25 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen 
-        name="(tabs)"
-        options={{
-          headerShown:false
-        }}
-      /> 
-      <Stack.Screen 
-        name="movies/[id]"
-        options={{
-          title: "Movie Details", 
-          headerShown: false,
-        }}
-      />
-    </Stack>  
+    <>
+      <StatusBar hidden={true}/>
+      <Stack>
+        <Stack.Screen 
+          name="(tabs)"
+          options={{
+            headerShown:false
+          }}
+        /> 
+        <Stack.Screen 
+          name="movies/[id]"
+          options={{
+            title: "Movie Details", 
+            headerShown: false,
+          }}
+        />
+      </Stack>  
+    </>
   )
-
-
 }
